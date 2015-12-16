434c03061b1d2cda9f9879b9484022a8079f7f03
Russell Schmidt <russell.schmidt@me.com>
2015-12-15 21:30:29 -0800

    add foundation

--- Gemfile
+++ Gemfile
@@ -30,9 +30,12 @@ gem 'coffee-rails', '~> 4.1.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+# Removed to work with Foundation
 gem 'turbolinks'
 
 # Install HAML for rails
 gem 'haml-rails', '~> 0.9'
 
 gem 'devise'
+
+gem 'foundation-rails'
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = '53173eb2493dfe89d@mailtrap.io'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
