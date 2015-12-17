bb372c943a87846af3465b4577e59b8e147e1dc5
Russell Schmidt <russell.schmidt@me.com>
2015-12-17 08:57:19 -0800

    add Wiki model, controller, views, change routes.rb to resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :wikis
+
   devise_for :users
-  root to: 'welcome#index'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
