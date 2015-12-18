b9d877cdb4ff5b67276c396fd39cbcd0e3cc95ee
Russell Schmidt <russell.schmidt@me.com>
2015-12-18 15:52:28 -0800

    add seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,22 @@ include RandomData
     password: RandomData.random_password
   )
 end
+
+# create an admin user
+User.create!(
+  email: "reuvenschmidt@gmail.com",
+  password: "12344321"
+)
+
 users = User.all
 puts "#{User.count} users created"
-# Create Wiki
+
+25.times do
+  Wiki.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    private: false,
+    user: users.sample
+  )
+end
+puts "#{Wiki.count} wikis created"
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Wiki, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.create!(email: RandomData.random_email, password: RandomData.random_password)}
+  let(:wiki) {Wiki.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false, user: user)}
+
+  describe "Wiki attributes" do
+    it "should respond to title" do
+      expect(wiki).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(wiki).to respond_to(:body)
+    end
+
+    it "should respond to private" do
+      expect(wiki).to respond_to(:private)
+    end
+
+    it "should respond to user" do
+      exepct(wiki).to respond_to(:user)
+    end
+  end
+
 end
