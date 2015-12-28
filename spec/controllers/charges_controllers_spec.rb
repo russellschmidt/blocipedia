9d425995531da1ae85bc9ac2e7a849693c22f1c2
Russell Schmidt <russell.schmidt@me.com>
2015-12-27 18:16:26 -0800

    add spec Stripe-Mock

--- spec/controllers/charges_controllers_spec.rb
+++ spec/controllers/charges_controllers_spec.rb
@@ -1,45 +1,52 @@
 require 'rails_helper'
-RSpec.describe WikisController, type: :controller do
-    let(:my_user) {create(:standard_user)}
+require 'stripe_mock'
 
-    describe "GET #new" do
-      context "for signed in user" do
-        before(:each) do
-          sign_in my_user
-        end
+RSpec.describe ChargesController, type: :controller do
+  let(:my_user) {create(:standard_user)}
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  before {StripeMock.start}
+  after {StripeMock.stop}
 
-        it "returns http success" do
-          get :new
-          expect(response).to have_http_status(:success)
-        end
+  describe "GET #new" do
+    context "for signed in user" do
+      before(:each) do
+        sign_in my_user
       end
 
-      context "for guest user" do
-        it "redirects to sign up page"  do
-          get :new
-          expect(response).to redirect_to(new_user_session_path)
-        end
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
       end
     end
 
-    describe "POST create" do
-      context "for signed in user" do
-        before(:each) do
-          sign_in my_user
-        end
+    context "for guest user" do
+      it "redirects to sign up page"  do
+        get :new
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 
-        it "returns http success" do
-          post :create, charge: {}
-          expect(response).to have_http_status(:success)
-        end
+  describe "POST create" do
+    context "for signed in user" do
+      before(:each) do
+        sign_in my_user
       end
 
-      context "for guest user" do
-        it "redirects to sign up page"  do
-          get :new
-          expect(response).to redirect_to(new_user_session_path)
-        end
+      it "creates a stripe customer" do
+        customer = Stripe::Customer.create({
+          email: my_user.email,
+          card: stripe_helper.generate_card_token
+        })
+        expect(customer.email).to eq(my_user.email)
       end
     end
 
+    context "for guest user" do
+      it "redirects to sign up page"  do
+        get :new
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
