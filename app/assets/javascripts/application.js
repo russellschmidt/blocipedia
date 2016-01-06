23e5743cb4aa2261ed6ff94ebd87e8266cf18708
Russell Schmidt <russell.schmidt@me.com>
2016-01-05 21:50:50 -0800

    another terrible attempt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require foundation
+//= require 'epiceditor'
 //= require blocipedia.js
---
+++ app/assets/javascripts/epiceditor.js
@@ -0,0 +1,38 @@
+//$(function(){ $(document).EpicEditor(); });
+var opts = {
+  container: 'epiceditor',
+  textarea: 'content-input',
+  basePath: '/assets',
+  clientSideStorage: true,
+  localStorageName: 'epiceditor',
+  useNativeFullscreen: true,
+  parser: marked,
+  file: {
+    name: 'epiceditor',
+    defaultContent: '',
+    autoSave: 100
+  },
+  theme: {
+    base: '/epiceditor.css',
+    preview: '/github.css',
+    editor: '/epic-style.css'
+  },
+  button: {
+    preview: true,
+    fullscreen: true,
+    bar: "auto"
+  },
+  focusOnLoad: false,
+  shortcut: {
+    modifier: 18,
+    fullscreen: 70,
+    preview: 80
+  },
+  string: {
+    togglePreview: 'Toggle Preview Mode',
+    toggleEdit: 'Toggle Edit Mode',
+    toggleFullscreen: 'Enter Fullscreen'
+  },
+  autogrow: false
+}
+var editor = new EpicEditor(opts).load();
